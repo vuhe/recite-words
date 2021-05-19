9bd0c344d3e65e332e3ca8812a1874b66f6b9493
vuhe <zhuhe202@qq.com>
2021-05-19 16:48:59 +0800

    fix(ex2): 添加计算器布局

---
+++ app/src/main/java/top/vuhe/android/CalculatorActivity.kt
@@ -0,0 +1,11 @@
+package top.vuhe.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
