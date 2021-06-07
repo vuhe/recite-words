d7d25720d68657aa75c30c38642ec35abd1ea556
vuhe <zhuhe202@qq.com>
2021-06-07 16:46:49 +0800

    feat(ex5): 完成实验5

--- app/src/main/java/top/vuhe/android/MainActivity.kt
+++ app/src/main/java/top/vuhe/android/MainActivity.kt
@@ -3,19 +3,20 @@ package top.vuhe.android
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import top.vuhe.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var account
-        get() = binding.accountEdit.text.toString()
-        set(value) = binding.accountEdit.setText(value)
+    private var name
+        get() = binding.nameEdit.text.toString()
+        set(value) = binding.nameEdit.setText(value)
 
-    private var password
-        get() = binding.passwordEdit.text.toString()
-        set(value) = binding.passwordEdit.setText(value)
+    private var phone
+        get() = binding.phoneEdit.text.toString()
+        set(value) = binding.phoneEdit.setText(value)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +24,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         // 设置根视图
         setContentView(binding.root)
-        // 记住密码恢复
-        val prefs = getPreferences(Context.MODE_PRIVATE)
-        account = prefs.getString("account", "")!!
-        password = prefs.getString("password", "")!!
+        // 初始化数据库
+        initSQLiteHelper(this)
 
-        // xml
-        binding.xmlLoginBtn.setOnClickListener {
-            WeatherActivity.actionStart(this, "xml")
+        binding.insertBtn.setOnClickListener {
+            val ans = if (insertData(Contact(name, phone))) "成功" else "失败"
+            Toast.makeText(this, "插入$ans", Toast.LENGTH_SHORT).show()
         }
-
-        // json
-        binding.jsonLoginBtn.setOnClickListener {
-            WeatherActivity.actionStart(this, "json")
+        binding.searchBtn.setOnClickListener {
+            val ans = if(searchData(Contact(name, phone))) "已" else "未"
+            Toast.makeText(this, "${ans}查询到数据存在", Toast.LENGTH_SHORT).show()
+        }
+        binding.deleteBtn.setOnClickListener {
+            deleteData(Contact(name, phone))
+            Toast.makeText(this, "删除成功", Toast.LENGTH_SHORT).show()
+        }
+        binding.updateBtn.setOnClickListener {
+            updateData(Contact(name, phone))
+            Toast.makeText(this, "更新成功", Toast.LENGTH_SHORT).show()
         }
     }
 }
